8f979fb7cd619d92b9091c4c7af4d968ddf55fcb
ncols <collignon.n@gmail.com>
2022-01-07 19:22:31 +0100

    Implement Add a project

---
+++ project-planner/src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="main-nav">
+      <router-link :to="{ name: 'Home' }">Projects</router-link>
+      <router-link :to="{ name: 'AddProject' }">Add New</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .main-nav {
+    text-align: center;
+    margin: 40px auto;
+  }
+  .main-nav a{
+    display: inline-block;
+    text-decoration: none;
+    margin: 0 10px;
+    color: rgb(139, 139, 139);
+    font-size: 18px;
+  }
+  a.router-link-active {
+    border-bottom: 2px solid #00ce89;
+    padding-bottom: 4px;
+    color: rgb(107, 107, 107);
+  }
+  a:hover {
+    color: rgb(80, 80, 80);
+    padding-bottom: 4px;
+    border-bottom: 2px solid #82b86d;
+  }
+</style>
