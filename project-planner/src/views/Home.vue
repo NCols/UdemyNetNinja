d4d7525a1dc18802c943038c5eb8fe1850387b3c
ncols <collignon.n@gmail.com>
2022-01-07 16:02:12 +0100

    JSON Server setup & display 2 project titles

--- project-planner/src/views/Home.vue
+++ project-planner/src/views/Home.vue
@@ -1,14 +1,30 @@
 <template>
   <div class="home">
-    Home
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <p>{{ project.title }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-  components: {
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((res) => res.json()) // Take the response, pass through json()
+      .then((data) => (this.projects = data)) // Take the json data returned and assign to 'projects' property
+      .catch((err) => console.log(err.message));
+  },
+};
 
-  }
-}
+// Other comments:
+// v-if="projects.length"  =>  show element only if projects.length is true => if projects is not empty
 </script>
